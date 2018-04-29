8c8ccd6498c92c0f52665186722eb8d73b249a0e
ruiqian <jianb1995@hotmail.com>
2018-04-29 22:33:05 +0800

    update type.h

--- mport/type.h
+++ mport/type.h
@@ -1,27 +1,29 @@
 #ifndef _MPORT_TYPE_H_
 #define _MPORT_TYPE_H_
 
-typedef unsigned char u8;
-typedef unsigned short u16;
-typedef unsigned long u32;
-typedef unsigned long long u64;
+#include <stdint.h>
 
-typedef signed char s8;
-typedef signed short s16;
-typedef signed long s32;
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
 
-typedef unsigned char __u8;
-typedef unsigned short __u16;
-typedef unsigned int __u32;
-typedef unsigned long long __u64;
+typedef int8_t  s8;
+typedef int16_t s16;
+typedef int32_t s32;
 
-typedef signed char __s8;
-typedef signed short __s16;
-typedef signed int __s32;
+typedef uint8_t  __u8;
+typedef uint16_t __u16;
+typedef uint32_t __u32;
+typedef uint64_t __u64;
 
-typedef unsigned char t_u8;
-typedef unsigned short t_u16;
-typedef unsigned int t_u32;
+typedef int8_t  __s8;
+typedef int16_t __s16;
+typedef int32_t __s32;
+
+typedef uint8_t  t_u8;
+typedef uint16_t t_u16;
+typedef uint32_t t_u32;
 
 typedef u16 __le16;
 typedef u16 __be16;
